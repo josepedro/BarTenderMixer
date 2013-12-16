868f7f7403d7291a02c690c8b1addde97eb9c2e4
Gepeto <1jpsneto@gmail.com>
2013-12-16 13:19:27 -0200

    Adcionando alteracoes em relacao a estrutura de nomes do projeto

--- bartendermixer/bar_tender_mixer/bar_tender_mixer/urls.py
+++ bartendermixer/bar_tender_mixer/bar_tender_mixer/urls.py
@@ -14,17 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^bar_tender_mixer/$', 'app.controller.consultabasica_controller.index'),
-    url(r'^bar_tender_mixer/$', 'app.controller.consultabasica_controller.consulta_por_regiao'),
-    url(r'^acidentes_rodovias/periodo$', 'app.controller.consultabasica_controller.consulta_por_periodo'),
-    url(r'^acidentes_rodovias/municipios-regiao$', 'app.controller.consultabasica_controller.consulta_municipios_na_regiao'),
-    url(r'^acidentes_rodovias/consulta/municipio$', 'app.controller.consultabasica_controller.consulta_ocorrencias_por_municipio'),
-    url(r'^acidentes_rodovias/consulta/periodo$', 'app.controller.consultabasica_controller.consulta_ocorrencias_por_periodo'),
-
-    url(r'^acidentes_rodovias/estatisticas/tipos$', 'app.controller.estatisticas_controller.tipos_acidentes'),
-    url(r'^acidentes_rodovias/estatisticas/causas-acidentes$', 'app.controller.estatisticas_controller.causas_acidentes'),
-    url(r'^acidentes_rodovias/estatisticas/ocorrencias-envolvidos$', 'app.controller.estatisticas_controller.ocorrencias_e_envolvidos'),
-    url(r'^acidentes_rodovias/estatisticas/acidentes-sexo$', 'app.controller.estatisticas_controller.acidentes_sexo'),
-    url(r'^acidentes_rodovias/estatisticas/br$', 'app.controller.estatisticas_controller.acidentes_br'),
-    url(r'^acidentes_rodovias/estatisticas/uf$', 'app.controller.estatisticas_controller.acidentes_uf'),
+    url(r'^bar_tender_mixer/$', 'app.controller.bar_controller.index'),
+    url(r'^bar_tender_mixer/1$', 'app.controller.bar_controller.opcao1'),
+    url(r'^bar_tender_mixer/2$', 'app.controller.bar_controller.opcao2')
 )
